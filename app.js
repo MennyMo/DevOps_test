421b9c7e1541345bb893549583b5d7fa216773a6
Molly <molade779@gmail.com>
2022-08-22 23:21:49 +0100

    fix esm issue

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongodb from './app/db/mongodb';
 import appInit from './app/config/app';
 
-async function init() {
+export async function init() {
    mongodb();
    appInit(express());
 }
